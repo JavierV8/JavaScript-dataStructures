d488ccbbaec480cc16c4699c188f7cc50303d38d
Javier Sanchez <javier.san.mail@gmail.com>
2021-12-21 10:47:30 +0100

    added comments stack and queue

--- 2-Stack.js
+++ 2-Stack.js
@@ -1,6 +1,6 @@
 /*
-Stack is a linear data structure that follows the LIFO(Last In First Out) 
-or FILO(First In Last Out) principle. It contains only one pointer the top 
+Stack is a linear data structure that follows the LIFO(Last In First Out)
+principle. It contains only one pointer the top 
 pointer that points to the topmost element of the stack. Whenever we add an 
 element to the stack, it is added at the top of the stack and also whenever 
 we delete an element from the stack it is deleted from the top of the stack.
@@ -15,27 +15,47 @@ class Stack {
         this.items = [];
     }
 
-    // push element into the items
+    /**
+     * push element into the items
+     * 
+     * @param {any} element 
+     */
     push(element) {
         this.items.push(element);
     }
 
-    // return top most element in the stack and removes it from the stack
+    /**
+     * return top most element in the stack and removes it from the stack
+     * 
+     * @returns {any} removed value
+     */
     pop() {
         if (this.items.length == 0) return "Underflow";
         return this.items.pop();
     }
 
-    // return the top most element from the stack but does'nt delete it.
+    /**
+     * return the top most element from the stack but does'nt delete it.
+     * 
+     * @returns {any} top most element from the stack
+     */
     peek() {
         return this.items[this.items.length - 1];
     }
 
-    // return true if stack is empty
+    /**
+     * return true if stack is empty
+     * 
+     * @returns {boolean}
+     */
     isEmpty() {
         return this.items.length == 0;
     }
 
+    /**
+     * 
+     * @returns {string}
+     */
     printStack() {
         var str = "";
         for (var i = 0; i < this.items.length; i++)
--- 3-queue.js
+++ 3-queue.js
@@ -6,33 +6,52 @@ front pointer: The front pointer contains the address of the starting element.
 rear pointer: The rear pointer contains the address of the last element of the queue.
 */
 class Queue {
-    // Array is used to implement a Queue
     constructor() {
         this.items = [];
     }
       
-    // adding element to the queue
+    /**
+     * adding element to the queue
+     * 
+     * @param {any} element 
+     */
     enqueue(element){    
         this.items.push(element);
     }
 
-    // removing element from the queue returns underflow when called on empty queue
+    /**
+     * removing element from the queue returns underflow when called on empty queue
+     * 
+     * @returns {any}
+     */
     dequeue() {
         if(this.isEmpty()) return "Underflow";
         return this.items.shift();
     }
 
-    // returns the Front element of the queue without removing it.
+    /**
+     * returns the Front element of the queue without removing it.
+     * 
+     * @returns {any}
+     */
     front() {
         if(this.isEmpty()) return "No elements in Queue";
         return this.items[0];
     }
 
-    // return true if the queue is empty.
+    /**
+     * return true if the queue is empty.
+     * 
+     * @returns {any}
+     */
     isEmpty() {
         return this.items.length == 0;
     }
 
+    /**
+     * 
+     * @returns {string}
+     */
     printQueue() {
         var str = "";
         for(var i = 0; i < this.items.length; i++)
